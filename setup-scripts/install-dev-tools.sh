79bb174d609e13712f49f5986d8490dab621057a
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2025-08-07 18:36:58 -0400

    f   - added lazygit

--- setup-scripts/install-dev-tools.sh
+++ setup-scripts/install-dev-tools.sh
@@ -8,4 +8,11 @@ sudo apt-get install -y \
     neovim \
     tmux \
 
+# Install lazygit (get latest version)
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v?([^"]+)".*/\1/')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm lazygit lazygit.tar.gz
+
 echo "Development tools installed successfully!"
